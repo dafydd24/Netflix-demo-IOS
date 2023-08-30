6aedb95e271e3a0df3589567d8a187ecead066d8
Patricio Mendoza <dafydd24@gmail.com>
2023-08-30 21:36:15 +0000

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by ec2-user on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
